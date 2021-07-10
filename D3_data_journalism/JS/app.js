dffc8970a86a93df8e29723a0bd45eb526e9481a
Fernando Nunez <79233160+Nunez77@users.noreply.github.com>
2021-07-10 18:34:12 -0500

    Set ups in app

--- D3_data_journalism/JS/app.js
+++ D3_data_journalism/JS/app.js
@@ -10,3 +10,31 @@
 // - Create and situate your axes and labels to the left and bottom of the chart.
 // - Note: You'll need to use python -m http.server to run the visualization.
 //   This will host the page at localhost:8000 in your web browser.
+
+// Set SVG dimensions and borders
+var svgW = 960
+var svgH = 620
+var margin = {
+    top: 20,
+    right: 40,
+    bottom: 200,
+    left: 100
+};
+
+// Determine chart's dimensions
+var chartW = svgW - margin.right - margin.left;
+var chartH = svgH - margin.top - margin.bottom;
+
+// Append SVG wrapper and group that will hold chart
+let svg = d3
+    .select(".chart")
+    .select("#scatter")
+    .append("svg")
+    .attr("class","chart")
+    .attr("width",svgW)
+    .attr("height",svgH);
+
+let chartGroup = svg.append("g") // The ‘g’ element is a container element for grouping together related graphics elements.
+    .attr("transform",`translate(${margin.left}, ${margin.top})`);
+
+// Open file and assign values to x and y axis.
